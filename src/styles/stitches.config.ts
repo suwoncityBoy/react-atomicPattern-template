464f07fbb61770323aabd10fdc7b7c11cdd012f5
이학경 <dlgkrrud00@naver.com>
2024-04-05 18:49:37 +0900

    design. 폰트스타일 적용,파비콘 적용

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -21,5 +21,18 @@ export const {
 
 export const globalStyles = globalCss({
   "*": { margin: 0, padding: 0, boxSizing: "border-box" },
-  body: { backgroundColor: "#fff" },
+  body: {
+    backgroundColor: "#fff",
+    fontFamily: "Pretendard-Regular, sans-serif",
+  },
+  "@font-face": [
+    {
+      fontFamily: "Pretendard-Regular",
+      src: `url('/fonts/Pretendard-Regular.woff2') format('woff2')`,
+    },
+    {
+      fontFamily: "Pretendard-Bold",
+      src: `url('/fonts/Pretendard-Bold.woff2') format('woff2')`,
+    },
+  ],
 });

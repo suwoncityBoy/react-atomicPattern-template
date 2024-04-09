d5717fd151b68083e19285add206fa511e4c5706
hakkyoung <dlgkrrud00@naver.com>
2024-04-09 20:22:43 +0900

    add. stitches ui 추가, dark/light json 파일 추가

---
+++ src/styles/ui/Column.ts
@@ -0,0 +1,11 @@
+import { styled } from '../stitches.config';
+
+export const Column = styled('div', {
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  flexWrap: 'wrap',
+  height: 'auto',
+});
---
+++ src/styles/ui/Row.ts
@@ -0,0 +1,11 @@
+import { styled } from '../stitches.config';
+
+export const Row = styled('div', {
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  width: 'auto',
+});

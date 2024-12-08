194280ea276e2061121cfdfe37dc9122c4b9ea5c
hakkyoung <dlgkrrud00@naver.com>
2024-12-08 17:51:48 +0900

    chore. atomic 폴더 분류

--- src/components/pages/AppRouter.tsx
+++ src/components/pages/AppRouter.tsx
@@ -1,11 +1,9 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Tab from '@/components/organisms/tab/Tab';
 import Calendar from '@/components/pages/calendar/Calendar';
 
 function AppRouter() {
   return (
     <>
-      <Tab />
       <Routes>
         <Route path='/' element={<Navigate replace to='/calendar' />} />
         <Route path='/calendar' element={<Calendar />} />
